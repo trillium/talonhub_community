02c1eb8e32855bb69108995349719fbe1e89c683
Trillium Smith <spiteless@gmail.com>
2023-11-27 16:34:11 -0800

    wip: Junk current repeater file, added elsewhere

--- plugin/repeater/repeater.py
+++ plugin/repeater/repeater.py
@@ -1,24 +1,9 @@
-# from talon import Context, Module, actions, app, clip, cron, ctrl, imgui, ui
-
-# @ctx.action("user.noise_trigger_pop")
-# def on_pop():
-#     core.repeat_phrase(1)
-
-from talon import Context, Module, actions, app, clip, cron, ctrl, imgui, ui, 
-
-ctx = Context()
-
-@ctx.action("user.noise_trigger_pop")
-def on_pop():
-    print("~~~~REPEATER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    core.repeat_phrase(1)
-
-# from talon import Context, Module, actions, app, clip, cron, ctrl, imgui, ui
+# from talon import Context, Module, actions, app, clip, cron, ctrl, imgui, ui, 
 
 # ctx = Context()
 
-# @ctx.action("user.noise_trigger_pop")
-# def on_pop():
-#     print("~~~~REPEATER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    
+# # @ctx.action("user.noise_trigger_pop")
+# # def on_pop():
+# #     print("~~~~REPEATER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+# #     core.repeat_phrase(1)
 
